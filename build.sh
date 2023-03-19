5de322808c76a2f905cbf9c61ff2f99ce4a7ec5d
mtl <mtl6906>
2023-03-20 00:32:28 +0800

    init

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd $1
+mkdir build
+cd build
+cmake ..
+make 
+sudo make install
---
+++ download.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/mtl6906/lsexception.git
+git clone https://github.com/mtl6906/lsrandom.git
+git clone https://github.com/mtl6906/lsorderedmap.git
+git clone https://github.com/mtl6906/lsio.git
+git clone https://github.com/mtl6906/lsbuffer.git
+git clone https://github.com/mtl6906/lspool.git
+git clone https://github.com/mtl6906/lscstring.git
+git clone https://github.com/mtl6906/lsalgorithm.git
+git clone https://github.com/mtl6906/lsitem.git
+git clone https://github.com/mtl6906/lscopyableitem.git
+git clone https://github.com/mtl6906/lsjson.git
+git clone https://github.com/mtl6906/lsconfig.git
+git clone https://github.com/mtl6906/lstime.git
+git clone https://github.com/mtl6906/lslogger.git
+git clone https://github.com/mtl6906/lsepoll.git
+git clone https://github.com/mtl6906/lsnet.git
+git clone https://github.com/mtl6906/lshttp.git
+git clone https://github.com/mtl6906/lsfile.git
+git clone --branch crazy https://github.com/mtl6906/lsrpc.git
+git clone https://github.com/mtl6906/lshttpprotocol.git
